6f8c107b1d3f84f4f78b20dd4f17c0e9dff756fa
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-17 17:04:13 -0500

    Finnaly works it can be improved

--- js/main.js
+++ js/main.js
@@ -1,50 +1,42 @@
-console.log('senity checks')
+ console.log('senity checks')
 
 
 
-document.getElementById("start").addEventListener("click", myFunction);
 
-function myFunction() {
-  document.getElementById("start").onCardClicked(event);
-} 
+  let  clickedCard = null;
+  let preventClick = false;
+  let pairFound = 0;
 
- 
+  function onCardClicked(event) {
+    const target = event.currentTarget;
+    if ( target.className.includes('done')){
+        return;
+    }
+    target.className = target.className.replace('color-hidden', '').trim();
 
-
- let  clickedCard = null;
- let preventClick = false;
- let pairFound = 0;
-
- function onCardClicked(event) {
-   const target = event.currentTarget;
-   if ( target.className.includes('done')){
-       return;
-   }
-   target.className = target.className.replace('color-hidden', '').trim();
-
-   if (!clickedCard) {
-  
-       clickedCard = target;
-     } else if (clickedCard) {
-       preventClick = false;
-       if (clickedCard.getAttribute('data-color') !== target.getAttribute('data-color')
-       ) {
-           // console log to check if works good or not
-           console.log('cards not the same')
-           clickedCard.className += 'done';
-           
-         setTimeout(() => {
-           clickedCard.className = clickedCard.className.replace('done', '').trim() + ' color-hidden';
-            target.className = target.className.replace('done', '').trim() + ' color-hidden';
-            clickedCard = null;
-            preventClick =false;
-           }, 200);
-       } else {
-           pairFound ++;
-         clickedCard = null;
-         if (pairFound === 6){
-             alert('You Win!! Congratulations!!');
-         }
-       }
-   }    
+    if (!clickedCard) {
+   
+        clickedCard = target;
+      } else if (clickedCard) {
+        preventClick = false;
+        if (clickedCard.getAttribute('data-color') !== target.getAttribute('data-color')
+        ) {
+            // console log to check if works good
+            console.log('cards not the same')
+            clickedCard.className += 'done';
+            
+          setTimeout(() => {
+            clickedCard.className = clickedCard.className.replace('done', '').trim() + ' color-hidden';
+             target.className = target.className.replace('done', '').trim() + ' color-hidden';
+             clickedCard = null;
+             preventClick =false;
+            }, 200);
+        } else {
+            pairFound ++;
+          clickedCard = null;
+          if (pairFound === 6){
+              alert('You Win!! Congratulations!!');
+          }
+        }
+    }    
 }
